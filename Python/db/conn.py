1424caf1b99d8b9c6f896a182bfc4436d78a5727
golanv <aburn02s@t-800.socialwork.illinois.edu>
2018-01-24 14:20:41 -0600

    Adds configuration file support for DB connections.

--- Python/db/conn.py
+++ Python/db/conn.py
@@ -1,11 +1,23 @@
 import psycopg2
 import getpass
 import sys
+import configparser
+
+
+# ConfigParser bits
+config = configparser.ConfigParser()
+config.read('connection.ini')
 
 # Bits to connect to Databases
+### Using ConfigParser ###
+#host = config['DEFAULT']['host']
+dbname = config['DEFAULT']['dbname']
+user = config['DEFAULT']['user']
+
+### Using user inputs ###
 host = input("Hostname: ")
-dbname = input("Database: ")
-user = input("Username: ")
+#dbname = input("Database: ")
+#user = input("Username: ")
 password = getpass.getpass(prompt="DB Password: ", stream=None)
 
 # Bits to determine what to sort (variables for table names).
