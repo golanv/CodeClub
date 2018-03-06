09eb251d1c050b4543c9a62a03c040f614e452fe
Aaron <aburn02s@t-800.socialwork.illinois.edu>
2018-03-06 17:00:07 -0600

    Adds MeaningfulDifference() and testing program.

---
+++ Java/src/MeaningfulDifference.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MeaningfulDifference {
+    
+    public MeaningfulDifference() {
+        
+    }
+ 
+    public double getUserInput(String prompt) {
+        Scanner input = new Scanner(System.in);
+        System.out.print(prompt + " " );
+        double moo = input.nextDouble();
+        return moo;
+    }
+    
+    public ArrayList collectData() {
+        ArrayList<Double> data = new ArrayList<Double>();
+        while (true) {
+            double i = getUserInput("Enter a number:");
+            if (i == 0) {
+                break;
+            } else {
+                //double num = Double.parseDouble(i);
+                data.add(i);
+            }            
+        }
+        return data;
+    }
+    
+    public ArrayList relDiff(ArrayList numbers) {
+        ArrayList<Double> diff = new ArrayList<Double>();
+        for (int i = 1; i < numbers.size(); i++) {
+            double x = (double)numbers.get(i);
+            double y = (double)numbers.get(i - 1);
+            diff.add((x - y) / y);
+        }
+        return diff;
+    }
+}
---
+++ Java/src/testMD.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class testMD {
+    public static void main(String[] args) {
+        new testMD();
+    }
+    
+    public testMD() {
+        MeaningfulDifference md = new MeaningfulDifference();
+        
+        // Test getUserInput();
+        //System.out.println(md.getUserInput("Enter a number:"));
+        
+        // Test collectData();
+        ArrayList list1 = md.collectData();
+        for (int i = 0; i < list1.size(); i++) {
+            //1 System.out.println(list1.get(i));
+        }
+        System.out.println(md.relDiff(list1));
+    }
+}
